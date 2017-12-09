03edcb1d371ad6df25e525605223595b112d14cf
William Elliott <wcelliott1991@gmail.com>
2017-12-09 12:16:58 -0500

    creates inial clue component to return values, questions and answers

---
+++ src/components/Clue.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Clue extends Component {
+    render() {
+        const { answer, question, value } = this.props.clue;
+
+        return (
+            <div className='clue'>
+                <h4>{value || 'unknown'}</h4>
+                <hr />
+                <h5>{question}</h5>
+                <hr />
+                <h5>{answer}</h5>
+            </div>
+        )
+    }
+}
+
+export default Clue;
