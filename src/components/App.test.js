26c95d93f1885dcd8e540af261c01dce64d1f16b
William Elliott <wcelliott1991@gmail.com>
2017-12-10 12:49:19 -0500

    passes test for correct link  title

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -14,5 +14,11 @@ describe('App', () => {
 
     it('creates the correct number of links', () => {
         expect(app.find('Link').length).toEqual(categories.length);
+    });
+
+    it('titles the links correctly', () => {
+        app.find('Link h4').forEach((linkTitle, index) => {
+            expect(linkTitle.text()).toEqual(categories[index].title);
+        })
     })
 });
